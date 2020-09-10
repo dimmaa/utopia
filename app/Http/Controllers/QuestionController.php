24f0abf36f9bc82626c1f39a2e10c3e908ecae47
dimmaa <dimma.hope@yandex.com>
2020-09-10 10:07:07 +0200

    implement form validatiob and store the question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AskQuestionRequest;
 use App\Question;
 use Illuminate\Http\Request;
 
@@ -39,9 +40,11 @@ class QuestionController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AskQuestionRequest $request)
     {
-        //
+        $request->user()->questions()->create($request->only('title', 'body'));
+
+        return redirect('questions')->with('success', "Your question has been submitted");
     }
 
     /**
