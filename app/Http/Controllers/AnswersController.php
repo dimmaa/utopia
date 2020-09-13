a98e323df909234845f17718a2ff20c8aaf8b868
dimmaa <dimma.hope@yandex.com>
2020-09-13 14:03:59 +0200

    implemented crud functionality for answer

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Answer;
 use App\Question;
 use Illuminate\Http\Request;
@@ -29,9 +30,11 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function edit(Answer $answer)
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+
+        return view('answers.edit', compact('question', 'answer'));
     }
 
     /**
@@ -41,9 +44,13 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Answer $answer)
+    public function update(Question $question, Request $request, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+        $answer->update($request->validate([
+            'body'=> 'required',
+        ]));
+        return redirect()->route('questions.show', $question->slug)->with('success','Your answer has been updated');
     }
 
     /**
