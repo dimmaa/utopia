69ad961ad6ae29ad5a72e30d72e828724486a6f4
dimmaa <dimma.hope@yandex.com>
2020-09-13 13:14:46 +0200

    Display answers for questions

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('questions', 'QuestionController')->except('show');
 Route::get('questions/{slug}', 'QuestionController@show')->name('questions.show');
-
+//Route::post('/questions/{question}/answers', 'AnswerController@store')->name('answers.store');
+Route::resource('questions.answers', 'AnswersController')->except(['index','create', 'show']);
